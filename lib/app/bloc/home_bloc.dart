4b947d8b51687917015607506632c3dfed58b105
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-11-21 04:43:26 -0300

    feat: implement HomeBloc for managing employee data state

---
+++ lib/app/bloc/home_bloc.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import '../models/employee_model.dart';
+import '../services/employers_service.dart';
+
+/// States
+base class HomeState {}
+
+final class HomeStateLoading extends HomeState {}
+
+final class HomeStateError extends HomeState {
+  final String message;
+
+  HomeStateError(this.message);
+}
+
+final class HomeStateIdle extends HomeState {
+  final List<EmployeeModel> employees;
+
+  HomeStateIdle(this.employees);
+}
+
+class HomeBloc {
+  final _state = StreamController<HomeState>();
+
+  final EmployersService _service;
+
+  HomeBloc(
+    this._service,
+  );
+
+  Stream<HomeState> get state => _state.stream.asBroadcastStream();
+
+  dispose() {
+    _state.close();
+  }
+
+  fetch() {
+    _state.add(HomeStateLoading());
+
+    _service.employees.then((result) {
+      result.fold((success) {
+        _state.add(HomeStateIdle(success));
+      }, (error) {
+        _state.add(HomeStateError(error.message));
+      });
+    });
+  }
+}
