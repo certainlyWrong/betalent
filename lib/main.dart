b0c78edeaaeddb2aee1a5292db14cb83f1ed368c
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-11-25 03:14:14 -0300

    feat: update main function to include AppLoadWidget and implement server connectivity check

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:betalent/app/utils/dio_config.dart';
 import 'package:flutter/material.dart';
 
+import 'app/app_load_widget.dart';
 import 'app/app_widget.dart';
 
-void main() {
-  runApp(const AppWidget());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AppLoadWidget());
+
+  /// Simulate a delay to check the server
+  await Future.delayed(const Duration(seconds: 3));
+
+  final (response, isCache) = await fetchData("http://localhost:5555/hello");
+  if (response.statusCode == 200) {
+    runApp(const AppWidget(isConnect: true));
+  } else {
+    runApp(const AppWidget(isConnect: false));
+  }
 }
